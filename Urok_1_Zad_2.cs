1ee19abcf9614e68c799c3dcbfbb4d2c4e39b822
Sergey Surikov Shooshpan <s1146611@yandex.ru>
2022-09-21 18:47:47 +0300

    zad_2 вернул

---
+++ Urok_1_Zad_2.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine("ВВедите значени числа А");
+int user_number_a= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите значени числа В");
+int user_number_b = Convert.ToInt32(Console.ReadLine());
+
+ {
+    if (user_number_a == user_number_b)
+   
+        Console.WriteLine("Числа равны");
+    
+    else if (user_number_a > user_number_b)
+    
+        Console.WriteLine($"Число А {user_number_a} Больше числа В {user_number_b}");
+   
+    else if (user_number_a < user_number_b)
+        
+        Console.WriteLine($"Число B {user_number_b} Больше числа A {user_number_a}");
+
+    else 
+
+        Console.WriteLine("Ошибка ввода чисел");
+
+}
