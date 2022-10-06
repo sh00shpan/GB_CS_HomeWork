6e36d9cbe4917c7e5adbb2e6868d40d5ec3a6889
sh00shpan <112971988+sh00shpan@users.noreply.github.com>
2022-10-07 01:32:19 +0300

    Задачи к уроку 4

---
+++ Urok4_zad27.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int InputUserNumber (int userNumber)
+{
+    Console.WriteLine("Введите число");
+    int number= Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Вы ввели число {number}");
+    return number;
+}
+
+void SumOfDigits (int numberToSum)
+{
+    int result = 0;
+    for (int n = numberToSum; n > 0; result += n % 10, n /= 10);
+    Console.WriteLine($"Сумма цифр данного числа {result}");
+}
+
+int Number = InputUserNumber(0);
+SumOfDigits(Number);
+
---
+++ Urok4_zad29.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 29: Напишите программу, в которой пользователь задает длину массива, 
+элементы которого задаются в диапазоне [1,99] и выводит на экран .
+5 -> [1, 2, 5, 7, 19]
+3 -> [6, 1, 33]
+*/
+
+
+int[] generateArray(int length, int start, int end)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+
+void printArray(int[] arrayToPrint)
+{
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write(arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+
+}
+
+Console.WriteLine("Введите длину массива");
+int DlinaMassiva = Convert.ToInt32(Console.ReadLine());
+
+int[] currentArray = generateArray(DlinaMassiva, 1, 99);
+printArray(currentArray);
+
