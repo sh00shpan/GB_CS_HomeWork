77b6449fa0c72721d2b5951423bff4c8e45c31a7
sh00shpan <112971988+sh00shpan@users.noreply.github.com>
2022-10-15 20:34:46 +0300

    Задачи к уроку 9

---
+++ Urok9_zad64.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+
+*/
+
+Console.WriteLine("Input number N");
+int UserNumber = Convert.ToInt32(Console.ReadLine());
+printRange(UserNumber);
+
+void printRange (int N)
+{
+    if (N == 0)
+    {
+        return;
+    }
+    printRange(N-1);
+    Console.Write($"{N}, ");   
+}
---
+++ Urok9_zad66.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+
+*/
+
+Console.WriteLine("Input start number M");
+int UserNumberStart = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input end number N");
+int UserNumberEnd = Convert.ToInt32(Console.ReadLine());
+
+printRangeSum(UserNumberStart, UserNumberEnd, 0);
+
+void printRangeSum (int Start, int End, int SumStart)
+{
+    if (Start == End)
+    {
+        SumStart=SumStart+End;
+        Console.Write($"сумма {SumStart}");
+        return;
+    }
+    SumStart=SumStart+End;
+    printRangeSum(Start, End-1,SumStart);
+    //Console.Write($"after rec {SumStart}, ");
+}
